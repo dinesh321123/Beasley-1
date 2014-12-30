fc1a90ea51a748b970add2966e7c2906103fc840
Faishal Saiyed <saiyedfaishal@gmail.com>
2014-12-31 05:02:17 +0530

    Fix styling issue and js error

--- themes/greatermedia/template-iframe.php
+++ themes/greatermedia/template-iframe.php
@@ -1,7 +1,5 @@
 <?php get_header( 'iframe' ); ?>
-	<main class="main" role="main">
-
+	<main class="main" role="main" id="gmr-iframe-main">
 		<?php get_template_part( 'content', get_post_format() ); ?>
-
 	</main>
 <?php get_footer( 'iframe' );
