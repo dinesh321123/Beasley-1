7495eea9d6704bb95fe6be53565fe2b5b72fc202
Eugene Manuilov <eugene.manuilov@10up.com>
2015-01-06 23:20:03 +0200

    Deactivated articles wrapping with "entry2-ad-wrap" div for "load-more" requests.

--- themes/greatermedia/partials/loop/front-page.php
+++ themes/greatermedia/partials/loop/front-page.php
@@ -1,14 +1,15 @@
 <?php
 
+$is_ajax = ! empty( $_GET['ajax'] );
 $post_count = 0;
 while ( have_posts() ) : the_post();
-	if ( 0 == ++$post_count % 5 ) :
+	if ( ! $is_ajax && 0 == ++$post_count % 5 ) :
 		?><div class='entry2-ad-wrap'><?php
 	endif;
 
 	get_template_part( 'partials/entry' );
 	
-	if ( 0 == $post_count % 5 ):
+	if ( ! $is_ajax && 0 == $post_count % 5 ):
 			?><div class='entry2-ad-wrap__ad mobile'>
 				<img src='http://placehold.it/180x150'>
 			</div>
