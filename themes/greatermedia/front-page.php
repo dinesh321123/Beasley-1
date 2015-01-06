1fa5005946a6d7ee15008b3eb4f764277e0af0dc
Taylor Dewey <td@tddewey.com>
2015-01-05 17:38:13 -0800

    Hot fixes an error on staging
    Fixes an issue where the entry partial was also checking to see if ads should be inserted, but it wasn't using variables that existed in the partial, causing a warning. If the warning itself was fixed, too many ads showed up.
    
    This fixes that warning, fixes a fatal that will show up in production (and on my local), and fixes the multiple ad slots issue that shows up after that.

--- themes/greatermedia/front-page.php
+++ themes/greatermedia/front-page.php
@@ -19,32 +19,28 @@ get_header();
 			get_template_part( 'partials/frontpage', 'highlights' );
 		?>
 
-			<section class="entries">				
-				<div class="ad__leaderboard desktop">
-					<img src='http://placehold.it/728x90'>
-					<?php // do_action( 'acm_tag', 'leaderboard-body' ); ?>
-				</div>
+			<section class="entries">
 
 				<h2 class="content__heading">Latest from WMGK</h2>
 
 				<?php $post_count = 0; ?>
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				
-					<?php 
-					if ( 0 == ++$post_count % 5 ): 
+
+					<?php
+					if ( 0 == ++$post_count % 5 ):
 					?>
 						<div class='entry2-ad-wrap'>
 					<?php endif; ?>
-				
+
 					<?php get_template_part( 'partials/entry' ); ?>
-					
+
 					<?php if ( 0 == $post_count % 5 ):	?>
 							<div class='entry2-ad-wrap__ad mobile'>
 								<img src='http://placehold.it/180x150'>
-							</div>						
+							</div>
 							<div class='entry2-ad-wrap__ad desktop'>
 								<img src='http://placehold.it/300x250'>
-							</div>						
+							</div>
 						</div>
 					<?php endif; ?>
 
