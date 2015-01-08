ce584f41b8f7ffba929efb0bdf779ad362dd0a02
Chancey Mathews <chancey.mathews@10up.com>
2015-01-08 10:52:17 -0500

    Temporarily remove feature/single-cleanup from staging.
    This reverts commit 1fc643948009c23b9c7e1d21167fcd326d7808f5, reversing
    changes made to 5011f089d7f549ea7c19a516cfb2e4499b7152a5.
    This reverts commit a1458c27198c780c3e01855544bbb40af5b77bce, reversing
    changes made to c4e51fe71c4189b778eec4d0b0241381c6a420f6.

--- themes/greatermedia/single-show.php
+++ themes/greatermedia/single-show.php
@@ -21,15 +21,13 @@
 							<div class="show__feature--primary">
 								<a href="<?php the_permalink(); ?>">
 									<div class="show__feature">
-										
-										<?php if ( has_post_thumbnail() ) : ?>
-											<div class='show-feature__thumbnail'>
+										<div class='show-feature__thumbnail'>
+											<?php if ( has_post_thumbnail() ) : ?>
 												<div class='thumbnail' style='background-image: url(<?php gm_post_thumbnail_url( 'gmr-show-featured-primary' ); ?>)'></div>
 											<?php else: ?>
 												<div class='thumbnail thumbnail-placeholder' style=''></div>
-											</div>
-										<?php endif; ?>
-										
+											<?php endif; ?>
+										</div>
 										<div class="show__feature--desc">
 											<div class='inner-wrap'>
 												<h3><?php the_title(); ?></h3>
