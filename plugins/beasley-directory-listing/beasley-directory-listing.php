90a72e47d99acfcdc5c58dcf6af9ad71fe5b7db6
Eugene Manuilov <eugene.manuilov@10up.com>
2018-01-29 20:07:31 +0200

    Added listing cpt

--- plugins/beasley-directory-listing/beasley-directory-listing.php
+++ plugins/beasley-directory-listing/beasley-directory-listing.php
@@ -6,3 +6,36 @@
  * Author: 10up
  * Author URI: http://10up.com/
  */
+
+function listings_init() {
+	$labels = array(
+		'name'                  => 'Listings',
+		'singular_name'         => 'Listing',
+		'add_new_item'          => 'Add New Listing',
+		'edit_item'             => 'Edit Listing',
+		'new_item'              => 'New Listing',
+		'view_item'             => 'View Listing',
+		'view_items'            => 'View Listings',
+		'search_items'          => 'Search Listings',
+		'not_found'             => 'No listings found',
+		'not_found_in_trash'    => 'No listings found in Trash',
+		'all_items'             => 'All Listings',
+		'archives'              => 'Listing Archives',
+		'attributes'            => 'Listing Attributes',
+		'insert_into_item'      => 'Insert into listing',
+		'uploaded_to_this_item' => 'Upload to this listing',
+	);
+
+	register_post_type( 'listing', array(
+		'label'         => 'Listings',
+		'labels'        => $labels,
+		'public'        => true,
+		'menu_position' => 21,
+		'menu_icon'     => 'dashicons-exerpt-view',
+		'has_archive'   => true,
+		'rewrite'       => array(
+			'slug' => apply_filters( 'beasley_listing_slug', 'listing' ),
+		),
+	) );
+}
+add_action( 'init', 'listings_init' );
