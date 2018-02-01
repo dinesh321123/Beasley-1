b6ac1a46a60ddd99396d100cb1c7a659d7f613f4
Eugene Manuilov <eugene.manuilov@10up.com>
2018-02-01 15:32:22 +0200

    Added ability to edit permalink settings

--- plugins/beasley-directory-listing/includes/Listings.php
+++ plugins/beasley-directory-listing/includes/Listings.php
@@ -17,6 +17,7 @@ class Listings {
 	 */
 	public function register() {
 		add_action( 'init', array( $this, 'register_cpt' ) );
+		add_action( 'admin_init', array( $this, 'register_settings' ) );
 
 		$this->register_featured_image( self::TAXONOMY_CATEGORY );
 	}
@@ -67,6 +68,40 @@ class Listings {
 			'public'        => true,
 			'show_tagcloud' => false,
 		) );
+	}
+
+	/**
+	 * Registers settings and checks if we need to save setting fields.
+	 *
+	 * @access public
+	 * @action admin_init
+	 */
+	public function register_settings() {
+		add_settings_field( 'listing-permalink', 'Directory Listing', array( $this, 'render_permalink_field' ), 'permalink', 'optional' );
+		register_setting( 'permalink', 'listing-permalink', 'sanitize_text_field' );
+
+		if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+			$nonce = filter_input( INPUT_POST, '__listing_nonce' );
+			if ( wp_verify_nonce( $nonce, 'listing-permalink' ) && current_user_can( 'manage_option' ) ) {
+				$permalink = filter_input( INPUT_POST, 'listing-permalink' );
+				update_option( 'listing-permalink', sanitize_text_field( $permalink ), 'no' );
+				flush_rewrite_rules();
+			}
+		}
+	}
+
+	/**
+	 * Renders permalink setting field.
+	 *
+	 * @access public
+	 */
+	public function render_permalink_field() {
+		wp_nonce_field( 'listing-permalink', '__listing_nonce', false );
+
+		printf(
+			'<input type="text" class="regular-text code" name="listing-permalink" value="%s">',
+			esc_attr( get_option( 'listing-permalink', '/directory/%listing-category%/%postname%/' ) )
+		);
 	}
 
 }
