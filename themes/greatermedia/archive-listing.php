00cd9f12dee8d7d9fd7b26fc2c0080193e5a0828
Ryan Sommers <ryan@10up.com>
2018-02-11 10:38:19 -0800

    Directory listing archive styling

--- themes/greatermedia/archive-listing.php
+++ themes/greatermedia/archive-listing.php
@@ -3,11 +3,21 @@
  * Archive template file for directory listing
  */
 
-get_header();
+get_header(); ?>
 
-echo '<div class="container">';
-	get_template_part( 'partials/directory-listing/archive-header' );
-	get_template_part( 'partials/directory-listing/archive-categories' );
-echo '</div>';
+	<section class="directory-archive">
+		<?php // TODO The root directory needs an image. ?>
+		<div class="directory-archive__hero" style="background-image: url('https://wmgk.gmr.dev/wp-content/uploads/sites/2/2017/01/shutterstock_519325135.jpg');">
+			<div class="directory-archive__title-wrapper">
+				<h1 class="directory-archive__title">What are you looking for?</h1>
+			</div>
+		</div>
 
-get_footer();
+		<div class="directory-archive__intro">
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque feugiat turpis eget efficitur mollis. Phasellus vitae tincidunt orci, quis convallis mauris. Proin nec felis at metus lobortis maximus eget nec massa. Donec ornare dictum vehicula. Curabitur eu dignissim elit. Mauris faucibus nisl at magna laoreet ullamcorper. Nam nec quam aliquet, fringilla leo tincidunt, convallis ante. Suspendisse vestibulum molestie dui vitae malesuada. Quisque sit amet massa sed nisl fringilla vehicula vel non ligula.</p>
+		</div>
+
+		<?php get_template_part( 'partials/directory-listing/archive-categories' ); ?>
+	</section>
+
+<? get_footer();
--- themes/greatermedia/taxonomy-listing-category.php
+++ themes/greatermedia/taxonomy-listing-category.php
@@ -9,18 +9,18 @@ $image_id = get_term_meta( $category->term_id, 'featured-image', true );
 get_header();
 
 ?>
-<section class="directory-category" role="article">
+<section class="directory-category">
 	<div class="directory-category__hero" style="background-image: url(<?php echo esc_url( wp_get_attachment_image_url( $image_id, 'gm-article-thumbnail' ) ); ?>">
 		<div class="directory-category__title-wrapper">
 			<h1 class="directory-category__title"><?php echo esc_attr( $category->name ); ?></h1>
 		</div>
 	</div>
 
-		<div class="directory-category__wrapper">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php get_template_part( 'partials/directory-listing/list-item' ); ?>
-			<?php endwhile; ?>
-		</div>
+	<div class="directory-category__wrapper">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'partials/directory-listing/list-item' ); ?>
+		<?php endwhile; ?>
+	</div>
 </section>
 <?php
 
