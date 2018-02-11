846eea0fb8fbf51fb29b067ae0fbf65b5a8e3f52
Ryan Sommers <ryan@10up.com>
2018-02-11 10:41:22 -0800

    Revert "Single listing header and content"
    This reverts commit 3abddceb50e947b560075331d6dd9d67582a6dcd.

--- themes/greatermedia/single-listing.php
+++ themes/greatermedia/single-listing.php
@@ -15,28 +15,21 @@ get_header();
 
 the_post();
 
-?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class( 'single-directory-listing' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+?><div class="container">
+	<div>
+		<?php the_post_thumbnail(); ?>
+	</div>
+	<div>
+		<?php if ( $image ) : ?>
+			<img src="<?php echo esc_url( $image ); ?>" alt="<?php echo esc_attr( $category->name ); ?>">
+		<?php endif; ?>
 
-		<div class="single-directory-listing__hero" style="background-image: url(<?php echo esc_url( get_the_post_thumbnail_url() ); ?>);"></div>
+		<h3><?php the_title(); ?></h3>
 
-		<header class="single-directory-listing__header">
-			<?php // TODO The mockups look like this should be a separate thumbnail set on the single post. The featured image is a car, this thumbnail is the brand logo. ?>
-			<div class="single-directory-listing__thumbnail">
-				<?php if ( $image ) : ?>
-					<img src="<?php echo esc_url( $image ); ?>" alt="<?php echo esc_attr( $category->name ); ?>">
-				<?php endif; ?>
-			</div>
-			<h1 class="single-directory-listing__title"><?php the_title(); ?></h1>
-		</header>
+		<?php the_content(); ?>
+	</div>
 
-		<div class="single-directory-listing__content">
-			<?php the_content(); ?>
-			<a class="inquire-link" href="<?php echo esc_url( get_permalink() ); ?>">Inquire</a>
-		</div>
+	<?php get_template_part( 'partials/directory-listing/related-items' ); ?>
+</div><?php
 
-		<?php get_template_part( 'partials/directory-listing/related-items' ); ?>
-
-	</article>
-
-<?php get_footer();
+get_footer();
