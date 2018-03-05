7272b08dea564115c63e98ec8f36d8ad90e1d3a3
Eugene Manuilov <eugene.manuilov@10up.com>
2018-03-05 15:32:09 +0200

    Added directory settings

--- plugins/beasley-directory-listing/includes/Directories.php
+++ plugins/beasley-directory-listing/includes/Directories.php
@@ -8,22 +8,12 @@ class Directories {
 	const TAXONOMY_CATEGORY = 'directory-category';
 	const TAXONOMY_TAG      = 'directory-tag';
 
-	/**
-	 * Registers hooks.
-	 *
-	 * @access public
-	 */
 	public function register() {
 		add_action( 'init', array( $this, 'register_cpt' ) );
+		add_action( 'init', array( $this, 'register_directory_settings' ) );
 		add_action( 'admin_menu', array( $this, 'register_menu' ) );
 	}
 
-	/**
-	 * Registers custom post type and taxonomies.
-	 *
-	 * @access public
-	 * @action init
-	 */
 	public function register_cpt() {
 		register_taxonomy( self::TAXONOMY_CATEGORY, array(), array(
 			'public'            => true,
@@ -40,27 +30,7 @@ class Directories {
 			'rewrite'           => false,
 		) );
 
-		$labels = array(
-			'name'                  => 'Directories',
-			'singular_name'         => 'Directory',
-			'add_new_item'          => 'Add New Directory',
-			'edit_item'             => 'Edit Directory',
-			'new_item'              => 'New Directory',
-			'view_item'             => 'View Directory',
-			'view_items'            => 'View Directories',
-			'search_items'          => 'Search Directories',
-			'not_found'             => 'No directories found',
-			'not_found_in_trash'    => 'No directories found in Trash',
-			'all_items'             => 'All Directories',
-			'archives'              => 'Directory Archives',
-			'attributes'            => 'Directory Attributes',
-			'insert_into_item'      => 'Insert into directory',
-			'uploaded_to_this_item' => 'Upload to this directory',
-		);
-
 		register_post_type( self::TYPE_DIRECTORIES, array(
-			'label'         => 'Directories',
-			'labels'        => $labels,
 			'public'        => false,
 			'show_ui'       => true,
 			'menu_position' => 52,
@@ -68,6 +38,24 @@ class Directories {
 			'has_archive'   => true,
 			'rewrite'       => false,
 			'supports'      => array( 'title' ),
+			'label'         => 'Directories',
+			'labels'        => array(
+				'name'                  => 'Directories',
+				'singular_name'         => 'Directory',
+				'add_new_item'          => 'Add New Directory',
+				'edit_item'             => 'Edit Directory',
+				'new_item'              => 'New Directory',
+				'view_item'             => 'View Directory',
+				'view_items'            => 'View Directories',
+				'search_items'          => 'Search Directories',
+				'not_found'             => 'No directories found',
+				'not_found_in_trash'    => 'No directories found in Trash',
+				'all_items'             => 'All Directories',
+				'archives'              => 'Directory Archives',
+				'attributes'            => 'Directory Attributes',
+				'insert_into_item'      => 'Insert into directory',
+				'uploaded_to_this_item' => 'Upload to this directory',
+			),
 		) );
 
 		$key = 'directories';
@@ -86,50 +74,94 @@ class Directories {
 		if ( ! empty( $directories ) ) {
 			foreach ( (array) $directories as $directory ) {
 				$directory = get_post( $directory );
-				if ( ! is_a( $directory, '\WP_Post' ) ) {
-					continue;
+				if ( is_a( $directory, '\WP_Post' ) ) {
+					register_post_type( 'directory-' . $directory->ID, array(
+						'public'        => true,
+						'menu_position' => 52,
+						'menu_icon'     => 'dashicons-exerpt-view',
+						'has_archive'   => true,
+						'rewrite'       => false,
+						'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+						'taxonomies'    => array( self::TAXONOMY_CATEGORY, self::TAXONOMY_TAG ),
+						'label'         => $directory->post_title,
+						'labels'        => array(
+							'name'                  => $directory->post_title,
+							'singular_name'         => $directory->post_title,
+							'add_new_item'          => 'Add New Item',
+							'edit_item'             => 'Edit Item',
+							'new_item'              => 'New Item',
+							'view_item'             => 'View Item',
+							'view_items'            => 'View Item',
+							'search_items'          => 'Search Item',
+							'not_found'             => 'No items found',
+							'not_found_in_trash'    => 'No items found in Trash',
+							'all_items'             => 'All Items',
+							'archives'              => 'Archives',
+							'attributes'            => 'Attributes',
+							'insert_into_item'      => 'Insert into post',
+							'uploaded_to_this_item' => 'Upload to this post',
+						),
+					) );
 				}
-
-				$labels = array(
-					'name'                  => $directory->post_title,
-					'singular_name'         => $directory->post_title,
-					'add_new_item'          => 'Add New Item',
-					'edit_item'             => 'Edit Item',
-					'new_item'              => 'New Item',
-					'view_item'             => 'View Item',
-					'view_items'            => 'View Item',
-					'search_items'          => 'Search Item',
-					'not_found'             => 'No items found',
-					'not_found_in_trash'    => 'No items found in Trash',
-					'all_items'             => 'All Items',
-					'archives'              => 'Archives',
-					'attributes'            => 'Attributes',
-					'insert_into_item'      => 'Insert into post',
-					'uploaded_to_this_item' => 'Upload to this post',
-				);
-
-				register_post_type( $directory->post_name, array(
-					'label'         => $directory->post_title,
-					'labels'        => $labels,
-					'public'        => true,
-					'menu_position' => 52,
-					'menu_icon'     => 'dashicons-exerpt-view',
-					'has_archive'   => true,
-					'rewrite'       => false,
-					'supports'      => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
-					'taxonomies'    => array( self::TAXONOMY_CATEGORY, self::TAXONOMY_TAG ),
-				) );
 			}
 		}
 	}
 
-	/**
-	 * Adds menu separator.
-	 *
-	 * @access public
-	 * @global array $menu
-	 * @param int $position
-	 */
+	public function register_directory_settings() {
+		if ( function_exists( 'acf_add_local_field_group' ) ) {
+			$location = array(
+				array(
+					array(
+						'param'    => 'post_type',
+						'operator' => '==',
+						'value'    => self::TYPE_DIRECTORIES,
+					),
+				),
+			);
+
+			acf_add_local_field_group( array(
+				'key'                   => 'group_5a7b2b84a6adb',
+				'title'                 => 'Settings',
+				'position'              => 'normal',
+				'style'                 => 'default',
+				'label_placement'       => 'top',
+				'instruction_placement' => 'label',
+				'active'                => 1,
+				'location'              => $location,
+				'fields'                => array(
+					array(
+						'key'          => 'field_5a9d41267af95',
+						'label'        => 'Archive Slug',
+						'name'         => 'archive_slug',
+						'type'         => 'text',
+						'instructions' => 'Enter directory archive slug which will be used as the root folder for all items in the directory.',
+						'required'     => 1,
+					),
+					array(
+						'key'           => 'field_5a9d43377af96',
+						'label'         => 'Hero Image',
+						'name'          => 'hero_image',
+						'type'          => 'image',
+						'instructions'  => 'Select a hero image for the directory homepage.',
+						'required'      => 1,
+						'return_format' => 'id',
+						'preview_size'  => 'thumbnail',
+						'library'       => 'all',
+					),
+					array(
+						'key'          => 'field_5a9d43977af97',
+						'label'        => 'Description',
+						'name'         => 'description',
+						'type'         => 'wysiwyg',
+						'instructions' => 'Directory welcome text which will be visible only on the directory archive page.',
+						'toolbar'      => 'full',
+						'media_upload' => 1,
+					),
+				),
+			) );
+		}
+	}
+
 	public function add_admin_menu_separator( $position ) {
 		global $menu;
 
@@ -148,12 +180,6 @@ class Directories {
 		ksort( $menu );
 	}
 
-	/**
-	 * Registers settings page.
-	 *
-	 * @access public
-	 * @action admin_menu
-	 */
 	public function register_menu() {
 		$this->add_admin_menu_separator( 51 );
 
