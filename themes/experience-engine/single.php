8b16930b0e60c9be407fb3a066caf6cfb368dc1a
dainemawer <daine.mawer@10up.com>
2018-12-03 11:50:06 -0600

    Single Article page styling

--- themes/experience-engine/single.php
+++ themes/experience-engine/single.php
@@ -1,31 +1,68 @@
 <?php get_header(); ?>
 
-<?php the_post(); ?>
+<div class="content-wrap">
+	<?php the_post(); ?>
 
-<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php get_template_part( 'partials/show/header' ); ?>
-	<h1><?php the_title(); ?></h1>
+	<div class="post-content">
+		<article id="post-<?php the_ID(); ?>" <?php post_class('post-article'); ?>>
 
-	<div>
-		<div>
+			<!-- Header -->
+			<header class="post-header">
+				<?php get_template_part( 'partials/show/header' ); ?>
+				<!-- Title -->
+				<h1 class="post-title"><?php the_title(); ?></h1>
+				<div class="post-meta">
+					<!-- Author -->
+					<div class="post-author">
+						<div class="post-author-name">
+							<?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
+							<?php the_author_meta( 'display_name' ); ?>
+
+							<div class="post-date">
+								<?php ee_the_date(); ?>
+							</div>
+
+						</div>
+					</div>
+
+					<!-- Share -->
+					<?php ee_the_share_buttons( get_permalink(), get_the_title() ); ?>
+				</div>
+			</header>
+
 			<div>
-				<span><?php the_author_meta( 'display_name' ); ?></span>
-				<span><?php ee_the_date(); ?></span>
-				<?php ee_the_share_buttons( get_permalink(), get_the_title() ); ?>
+				<div>
+					<!-- Featured Media -->
+					<figure class="post-thumbnail">
+						<?php get_template_part( 'partials/featured-media' ); ?>
+					</figure>
+
+					<div class="post-thumbnail-caption">
+						<?php echo get_the_post_thumbnail_caption( get_the_ID() ); ?>
+					</div>
+
+					<?php ee_the_content_with_ads(); ?>
+
+					<!-- Categories -->
+					<?php get_template_part( 'partials/content/categories' ); ?>
+
+					<!-- Tags -->
+					<?php get_template_part( 'partials/content/tags' ); ?>
+				</div>
+
+				
 			</div>
+		</article>
 
-			<?php get_template_part( 'partials/featured-media' ); ?>
-
-			<?php ee_the_content_with_ads(); ?>
-
-			<?php get_template_part( 'partials/content/categories' ); ?>
-			<?php get_template_part( 'partials/content/tags' ); ?>
-		</div>
-
-		<?php get_template_part( 'partials/ads/sidebar-sticky' ); ?>
+		<!-- Sidebar -->
+		<aside class="post-sidebar">
+			<?php get_template_part( 'partials/ads/sidebar-sticky' ); ?>
+		</aside>
 	</div>
 
+	<!-- Related Articles -->
 	<?php get_template_part( 'partials/related-articles' ); ?>
+
 </div>
 
 <?php get_footer(); ?>
