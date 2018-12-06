dd14448defaab836e2792d4a664e15114ef9388f
dainemawer <daine.mawer@10up.com>
2018-12-06 14:27:36 -0600

    Styling for single article

--- themes/experience-engine/single.php
+++ themes/experience-engine/single.php
@@ -8,25 +8,29 @@
 
 			<!-- Header -->
 			<header class="post-header">
+
 				<?php get_template_part( 'partials/show/header' ); ?>
-				<!-- Title -->
-				<h1 class="post-title"><?php the_title(); ?></h1>
+
+				<h1 class="post-title">
+					<?php the_title(); ?>
+				</h1>
+
 				<div class="post-meta">
-					<!-- Author -->
-					<div class="post-author">
-						<div class="post-author-name">
-							<?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
-							<?php the_author_meta( 'display_name' ); ?>
-
-							<div class="post-date">
-								<?php ee_the_date(); ?>
+					<div class="post-meta-author">
+							<div class="post-author-name">
+								<div class="post-author-data">
+									<?php echo get_avatar( get_the_author_meta( 'ID' ), 40 ); ?>
+									<?php the_author_meta( 'display_name' ); ?>
+								</div>
+								
+								<div class="post-meta-date">
+									<?php ee_the_date(); ?>
+								</div>
 							</div>
-
-						</div>
 					</div>
 
-					<!-- Share -->
 					<?php ee_the_share_buttons( get_permalink(), get_the_title() ); ?>
+
 				</div>
 			</header>
 
@@ -37,20 +41,27 @@
 						<?php get_template_part( 'partials/featured-media' ); ?>
 					</figure>
 
-					<div class="post-thumbnail-caption">
-						<?php echo get_the_post_thumbnail_caption( get_the_ID() ); ?>
+					<div class="post-content-wrap">
+						<div class="post-thumbnail-caption">
+							<?php echo get_the_post_thumbnail_caption( get_the_ID() ); ?>
+						</div>
 					</div>
 
-					<?php ee_the_content_with_ads(); ?>
+					<div class="post-content-wrap">
 
-					<!-- Categories -->
-					<?php get_template_part( 'partials/content/categories' ); ?>
+						<?php ee_the_content_with_ads(); ?>
 
-					<!-- Tags -->
-					<?php get_template_part( 'partials/content/tags' ); ?>
+						<div class="post-author-description">
+							<?php echo get_the_author_meta( 'description' ); ?>
+						</div>
+
+						<!-- Categories -->
+						<?php get_template_part( 'partials/content/categories' ); ?>
+
+						<!-- Tags -->
+						<?php get_template_part( 'partials/content/tags' ); ?>
+					</div>
 				</div>
-
-				
 			</div>
 		</article>
 
