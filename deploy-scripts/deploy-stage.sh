21f7178124a183475ccd5a0060f5e075a5a6429a
David Greenwald <david@davidgreenwald.com>
2020-08-04 11:02:51 -0700

    Remove dry-run

--- deploy-scripts/deploy-stage.sh
+++ deploy-scripts/deploy-stage.sh
@@ -6,7 +6,6 @@ set -x
 # Catch Errors
 set -euo pipefail
 
-# adding --dry-run flag until stage2 branch is ready to use for stage deploys
-rsync --dry-run -vrxc --delay-updates --delete-after ./ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/ --exclude-from=./deploy-scripts/rsync-excludes.txt
+rsync -vrxc --delay-updates --delete-after ./ beanstalk@54.87.4.54:/var/www/html/wordpress/wp-content/ --exclude-from=./deploy-scripts/rsync-excludes.txt
 
 set +x
