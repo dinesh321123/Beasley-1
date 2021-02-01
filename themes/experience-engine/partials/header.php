c03effe79011de31fe014dc567792497d1126027
mikepersico <mike.persico@bbgi.com>
2021-02-01 09:31:16 -0500

    Increase Desktop Sublogo Width.

--- themes/experience-engine/partials/header.php
+++ themes/experience-engine/partials/header.php
@@ -14,7 +14,7 @@
 		<div class="logo" itemscope itemtype="http://schema.org/Organization">
 			<?php ee_the_custom_logo( 154, 88 ); ?>
 			<?php ee_the_subheader_logo( 'desktop', 154, 88 ); ?>
-			<?php ee_the_subheader_logo( 'mobile', 154, 88 ); ?>
+			<?php ee_the_subheader_logo( 'mobile', 462, 88 ); ?>
 			<span class="screen-reader-text"><?php wp_title(); ?></span>
 		</div>
 
