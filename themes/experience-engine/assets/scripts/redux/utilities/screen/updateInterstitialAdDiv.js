01d304074377b6d33241d49e661df88312993617
mikepersico <mike.persico@bbgi.com>
2021-02-22 11:45:43 -0500

    Put in timeout to hide Interstitial Div that has no children.

--- themes/experience-engine/assets/scripts/redux/utilities/screen/updateInterstitialAdDiv.js
+++ themes/experience-engine/assets/scripts/redux/utilities/screen/updateInterstitialAdDiv.js
@@ -7,9 +7,22 @@ export default function updateInterstitialAdDiv() {
 		);
 
 		if (interstitialAdDiv) {
+			const copyOfInterstitialStyle = `${interstitialAdDiv.style.cssText}`;
+
 			// Open Div To Full Screen So That Lazy Ad Can Show
 			interstitialAdDiv.style.cssText =
 				'bottom: 0; height: 100%; left: 0; position: fixed; right: 0; top: 0; width: 100%; z-index: 9000003;';
+
+			// Put in watchdog to revert style should Google not create children in 3.5 seconds
+			setTimeout(() => {
+				console.log('Checking whether Interstital has content.');
+				if (interstitialAdDiv.children.length === 0) {
+					console.log(
+						`Reverting Interstital style to ${copyOfInterstitialStyle}`,
+					);
+					interstitialAdDiv.style.cssText = copyOfInterstitialStyle;
+				}
+			}, 3500);
 		}
 	}
 }
