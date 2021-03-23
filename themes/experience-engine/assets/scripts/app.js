cb628725b7a622dab080e3114ca6cc486dc479c2
Nikunj Modi <Nikunjmodi@Nikunjs-Mac-mini.local>
2021-03-23 20:06:44 +0530

    call function in app.js file to create cookies

--- themes/experience-engine/assets/scripts/app.js
+++ themes/experience-engine/assets/scripts/app.js
@@ -1,17 +1,17 @@
 import React, { useEffect } from 'react';
-
 import { IntersectionObserverProvider } from './context';
 import {
 	ContentDispatcher,
 	ModalDispatcher,
 	LivePlayer,
 	PrimaryNav,
-	UserNav,
+	UserNav, 
 	SearchForm,
 } from './modules';
 import BackToTop from './components/BackToTop';
 import ErrorBoundary from './components/ErrorBoundary';
 import { isSafari, isWindowsBrowser } from './library';
+import { uniqueUserId } from './library/uniqueuserid';
 
 /**
  * The App's entry point.
@@ -23,8 +23,8 @@ const App = () => {
 		} else if (isWindowsBrowser()) {
 			document.body.classList.add('is-windows');
 		}
+		uniqueUserId()
 	}, []);
-
 	return (
 		<IntersectionObserverProvider>
 			<ErrorBoundary>
