4f70e0084a8622e59f981e8873bfc8fe58c13bf7
Rupesh Jorkar <rupesh.jorkar@bbgi.com>
2021-07-01 17:43:19 +0530

    Did unit testing, Resolved bugs and removed commented code

--- plugins/cpt-tag-permissions/includes/tag-permissions-metaboxes.php
+++ plugins/cpt-tag-permissions/includes/tag-permissions-metaboxes.php
@@ -29,9 +29,7 @@ class TagPermissionsMetaboxes {
 		global $typenow, $pagenow;
 		$post_types = self::tag_permissions_posttype_list();
 	   if ( in_array( $typenow, $post_types ) && in_array( $pagenow, array( 'post.php', 'post-new.php' ) ) ) {
-	      // $postfix = ( defined( 'SCRIPT_DEBUG' ) && true === SCRIPT_DEBUG ) ? '' : '.min';
-		   // wp_enqueue_script( 'jquery-ui-autocomplete', "http://beasley.test/wp-includes/js/jquery/ui/autocomplete.min.js?ver=1.11.4" );
-			if ( ! current_user_can( 'manage_categories' ) ) {
+	       if ( ! current_user_can( 'manage_categories' ) ) {
 				wp_register_style('tag-permissions-admin', TAG_PERMISSIONS_URL . "assets/css/tp_admin.css", array(), TAG_PERMISSIONS_VERSION, 'all');
 				wp_enqueue_style('tag-permissions-admin');  	
 		   		wp_enqueue_script( 'tag-permissions-admin', TAG_PERMISSIONS_URL . "assets/js/tp_admin.js", array('jquery'), TAG_PERMISSIONS_VERSION, true);
@@ -40,10 +38,8 @@ class TagPermissionsMetaboxes {
 	   }
    }
 
-
 	public static function tag_permissions_posttype_list() {
-		return (array) apply_filters( 'tag-permissions-allow-post-types', array( 'post', 'gmr_gallery', 'show', 'gmr_album', 'tribe_events', 'announcement', 'contest', 'podcast', 'episode', 'content-kit' )  );
-		// return array( 'post', 'gmr_gallery', 'show', 'gmr_album', 'tribe_events', 'subscription' );
+		return (array) apply_filters( 'tag-permissions-allow-post-types', array( 'post', 'listicle_cpt', 'gmr_gallery', 'show', 'gmr_album', 'tribe_events', 'announcement', 'contest', 'podcast', 'episode', 'content-kit' )  );
 	}
 
 	public static function tags_meta_box_remove() {
@@ -61,42 +57,32 @@ class TagPermissionsMetaboxes {
 	 * @param $post_type
 	 */
 	public static function add_meta_box( $post_type ) {
-		/* echo current_user_can('administrator');
-		$user = wp_get_current_user(); // getting & setting the current user 
-		$roles = ( array ) $user->roles; // obtaining the role 
-		print_r($user);
-  exit; */
 		if ( ! current_user_can( 'manage_categories' ) ) {
 			$post_types = self::tag_permissions_posttype_list();
 			if ( in_array( $post_type, $post_types ) ) {
-				add_meta_box( 'tag-permissions-post_tag', 'Custom Tags', array( __CLASS__, 'render_footer_metabox' ), $post_type, 'side', 'core' );
+				add_meta_box( 'tag-permissions-post_tag', 'Manage Tags', array( __CLASS__, 'render_footer_metabox' ), $post_type, 'side', 'core' );
 			}
 		}
-
 	}
+
 	public static function remove_first_space( $val ){
 		return trim( $val );
 	}
 
 	public static function is_tag_available() {
 		$output = array();
-		
 		if ( ! empty( $_POST['tags_data'] ) && $_POST['tags_data'] != '' ) {
-			// print_r( $_REQUEST );
 			$available_tag_html = array();
 			$available_tag_array = array();
 			$not_available_tag_array = array();
 			
 			if( !empty( $_POST['tags_data'] ) ){
-
 				$tags_array = explode( ",", $_POST['tags_data'] );
 				$prior_tags_array = explode( ",", $_POST['prior_tags_data'] );
 				$new_tags_array_without_first_space = array_map( array( __CLASS__, 'remove_first_space' ), $tags_array );
 				$prior_tags_array_without_first_space = array_map( array( __CLASS__, 'remove_first_space' ), $prior_tags_array );
 
 				if( isset( $_POST['activity'] ) && $_POST['activity'] == 'remove' ){
-					// print_r( $tags_array );
-					// print_r( $prior_tags_array );
 					if ( ( $key = array_search( $tags_array[0], $prior_tags_array ) ) !== false) {
 						unset($prior_tags_array[$key]);
 						$merge_tag_array = $prior_tags_array;
@@ -104,39 +90,24 @@ class TagPermissionsMetaboxes {
 				} else {
 					$merge_tag_array = array_unique( array_merge( $new_tags_array_without_first_space, $prior_tags_array_without_first_space ) );
 				}
-				
-				
-
-				// print_r($merge_tag_array);
-				// echo "<pre> ", print_r( $tags_array ), print_r($prior_tags_array_without_first_space), " ---- Final array ---- ", print_r( $new_tags_array );
 				$counter = 0;
 				foreach( $merge_tag_array as $tag_name ){
 					if( !empty( $tag_name ) && !ctype_space( $tag_name ) && $tag_name != "" ) {
-						// echo ctype_space( $tag_name ), ' -- ';
-						// echo $tag_name.' -- ';
 						$tag_status = term_exists( $tag_name, 'post_tag' );
-						// print_r( $tag_status );
 						if( !empty( $tag_status ) ) {
 							$available_tag_array[] = $tag_name;
-							// $available_tag_html[] = '<li><button type="button" id="post_tag-check-num-'. $counter .'" class="tp_ntdelbutton ntdelbutton"><span class="remove-tag-icon" aria-hidden="true"></span><span class="screen-reader-text">Remove term: '. ltrim($tag_name) .'</span></button>&nbsp;'. ltrim($tag_name) .'</li>';
 							$available_tag_html[] = '<li><button type="button" id="post_tag-check-num-'. $counter .'" class="ntdelbutton" value="'. trim($tag_name) .'"><span class="remove-tag-icon" aria-hidden="true"></span><span class="screen-reader-text">Remove term: '. trim($tag_name) .'</span></button>'. trim($tag_name) .'</li>';
 							$counter++;
 						} else {
-							// echo $tag_name.'  --- '; 
-							// $not_available_tag_array[] = '<p>'.$tag_name.' tag not available</p>';
 							$not_available_tag_array[] = $tag_name;
 						}
 					}
 				}
-
-				
 			}
-			// print_r($not_available_tag_array);
 			$output['available_tag_html'] = array_unique( $available_tag_html );
 			$output['available_tag_string'] = implode( ",", $available_tag_array );
 			$output['not_available_tag_string'] = $not_available_tag_array;
 		}
-		// print_r( $output );
 		wp_send_json( $output );
 	}
 
@@ -144,7 +115,6 @@ class TagPermissionsMetaboxes {
 		wp_nonce_field( '_tag_permissions_nonce', '_tag_permissions_nonce' );
 		$tags_array = get_the_tags( $post->ID );
 		$tags_string = "";
-		// $am_footer_description = self::tp_get_metavalue( 'am_footer_description' );
 		?>
 			<div class="inside">
 				<div class="tagsdiv" id="post_tag">
@@ -167,7 +137,6 @@ class TagPermissionsMetaboxes {
 								$tag_count = 0;
 								foreach($tags_array as $tag) {
 									$tags_string[] = $tag->name;
-									// echo '<li><button type="button" id="post_tag-check-num-'. $tag->name .'" class="tp_ntdelbutton ntdelbutton"><span class="remove-tag-icon" aria-hidden="true"></span><span class="screen-reader-text">Remove term: '. $tag->name .'</span></button>&nbsp;'. $tag->name .'</li>';
 									echo '<li><button type="button" id="post_tag-check-num-'. $tag_count .'" class="ntdelbutton" value="'. $tag->name .'" ><span class="remove-tag-icon" aria-hidden="true"></span><span class="screen-reader-text">Remove term: '. $tag->name .'</span></button>&nbsp;'. $tag->name .'</li>';
 									$tag_count++;
 								}
@@ -175,8 +144,7 @@ class TagPermissionsMetaboxes {
 							}
 						?>
 					</ul>
-					<div class="">
-					<?php //nojs-tags hide-if-js?>
+					<div class="nojs-tags hide-if-js">
 							<label for="tax-input-post_tag">Add or remove tags</label>
 							<p>
 								<textarea name="tag_permissions_post_tag" rows="3" cols="20" class="the-tags" id="tag_permissions_post_tag" aria-describedby="new-tag-post_tag-desc"> <?php echo $tags_string;?> </textarea>
@@ -195,16 +163,6 @@ class TagPermissionsMetaboxes {
 		if ( isset( $_POST['tag_permissions_post_tag'] ) ) {
 			$post_tag_array = explode( ',', $_POST['tag_permissions_post_tag'] );
 			wp_set_post_tags( $post_id, $post_tag_array); // Set tags to Post
-		}
-	}
-
-	public static function tp_get_metavalue( $value ) {
-		global $post;
-		$field = get_post_meta( $post->ID, $value, true );
-		if ( ! empty( $field ) ) {
-			return is_array( $field ) ? stripslashes_deep( $field ) : stripslashes( wp_kses_decode_entities( $field ) );
-		} else {
-			return false;
 		}
 	}
 }
