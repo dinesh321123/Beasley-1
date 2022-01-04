3be8a7d9e6b4280a0c74446f0c4f4feb1d31bcc1
mikepersico <mike.persico@bbgi.com>
2022-01-04 07:50:03 -0500

    Functional POC.

--- themes/experience-engine/firebase-messaging-sw.js
+++ themes/experience-engine/firebase-messaging-sw.js
@@ -1,3 +1,31 @@
+/* eslint-disable */
 // firebase-messaging-sw.js
 
-console.log('FIREBASE MESSAGING');
+console.log('FIREBASE MESSAGING SW');
+
+if (typeof importScripts === 'function') {
+	importScripts(
+		'https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js',
+	);
+	importScripts(
+		'https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js',
+	);
+
+	const firebaseConfig = {
+		apiKey: 'AIzaSyDvXlmIDLb2A65S-ZF9pBpsqwHq6JHDGcg',
+		authDomain: 'bbgi-experience-staging.firebaseapp.com',
+		databaseURL: 'https://bbgi-experience-staging.firebaseio.com',
+		projectId: 'bbgi-experience-staging',
+		storageBucket: 'bbgi-experience-staging.appspot.com',
+		messagingSenderId: '3738444381',
+		appId: '1:3738444381:web:0887af7963e0de2459a3b4',
+	};
+	firebase.initializeApp(firebaseConfig);
+
+	const messaging = firebase.messaging();
+	messaging.onBackgroundMessage(payload => {
+		console.log('[firebase-messaging-sw.js] onBackgroundMessage - ', payload);
+	});
+
+	console.log('FIREBASE MESSAGING SW LOADED');
+}
