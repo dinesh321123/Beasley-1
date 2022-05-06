75cbcb78f5d06eddfada4b78b9938d6893171c56
kevingilper <dpc12pkd3blindmice!>
2022-05-06 15:51:18 -0400

    remove tag line

--- themes/experience-engine-v2/partials/hero/checkpoint.php
+++ themes/experience-engine-v2/partials/hero/checkpoint.php
@@ -4,9 +4,6 @@
         <span>
             <img class="cp-main-image" />
             <div class="cp-cta-button-container">
-				<div class="cp-tag-line">
-					YOUR <em>CHECKPOINT</em> FOR ESPORTS AND GAMING
-				</div>
                 <a class="cp-watch-button" href="https://www.youtube.com/channel/UC9yKA84HoOmDV5r6WLrwm9A">SUBSCRIBE</a>
             </div>
         </span>
