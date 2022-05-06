7b3774cd41305f4911d065c46bd8f5c7e234de75
kevingilper <dpc12pkd3blindmice!>
2022-05-06 15:00:15 -0400

    tag line checkpoint hero

--- themes/experience-engine-v2/partials/hero/checkpoint.php
+++ themes/experience-engine-v2/partials/hero/checkpoint.php
@@ -4,7 +4,10 @@
         <span>
             <img class="cp-main-image" />
             <div class="cp-cta-button-container">
-                <a class="cp-watch-button" href="https://www.youtube.com/channel/UCR9NWgab4omJxG79m0BLvag">SUBSCRIBE</a>
+				<div class="cp-tag-line">
+					YOUR <em>CHECKPOINT</em> FOR ESPORTS AND GAMING
+				</div>
+                <a class="cp-watch-button" href="https://www.youtube.com/channel/UC9yKA84HoOmDV5r6WLrwm9A">SUBSCRIBE</a>
             </div>
         </span>
 	</div>
