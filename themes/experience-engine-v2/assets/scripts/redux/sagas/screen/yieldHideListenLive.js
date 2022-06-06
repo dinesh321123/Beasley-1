22127884791ee75bc2cc527af4793d52ea0c6db3
mikepersico <mike.persico@bbgi.com>
2022-06-06 14:18:26 -0400

    Show Sign In Modal every time Listen Live closes while player is Live Playing.

--- themes/experience-engine-v2/assets/scripts/redux/sagas/screen/yieldHideListenLive.js
+++ themes/experience-engine-v2/assets/scripts/redux/sagas/screen/yieldHideListenLive.js
@@ -1,6 +1,8 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { put, select, takeLatest } from 'redux-saga/effects';
 import { ACTION_HIDE_LISTEN_LIVE } from '../../actions/screen';
 import { hideDropdownAd } from '../../actions/dropdownad';
+import { STATUSES } from '../../actions/player';
+import { showSignInModal } from '../../actions/modal';
 
 /**
  * Generator runs whenever [ ACTION_HIDE_LISTEN_LIVE ]
@@ -10,6 +12,11 @@ function* yieldHideListenLive() {
 	yield put(hideDropdownAd());
 	const listenlivecontainer = document.getElementById('my-listen-dropdown2');
 	listenlivecontainer.style.display = 'none';
+
+	const playerStore = yield select(store => store.player);
+	if (playerStore.status === STATUSES.LIVE_PLAYING) {
+		yield put(showSignInModal());
+	}
 }
 
 /**
--- themes/experience-engine-v2/assets/scripts/redux/sagas/screen/yieldPlayerStatusChange.js
+++ themes/experience-engine-v2/assets/scripts/redux/sagas/screen/yieldPlayerStatusChange.js
@@ -7,11 +7,11 @@ import { ACTION_STATUS_CHANGE, STATUSES } from '../../actions/player';
  * is dispatched.
  * Fires hideListenLive() if Live Playing and NOT disallowing Listen Live Auto Close
  */
-function* yieldAutoHideHideListenLive() {
+function* yieldPlayerStatusChange() {
 	const playerStore = yield select(store => store.player);
 	if (playerStore.status === STATUSES.LIVE_PLAYING) {
 		const screenStore = yield select(store => store.screen);
-		if (screenStore.isAllowingListenLiveAutoClose) {
+		if (screenStore.isAutoClosingListenLiveMode) {
 			const delay = ms => new Promise(res => setTimeout(res, ms));
 			console.log('hiding in 5 sec');
 			yield delay(5000);
@@ -24,5 +24,5 @@ function* yieldAutoHideHideListenLive() {
  * Generator used to bind action and callback
  */
 export default function* watchAutoHideListenLive() {
-	yield takeLatest([ACTION_STATUS_CHANGE], yieldAutoHideHideListenLive);
+	yield takeLatest([ACTION_STATUS_CHANGE], yieldPlayerStatusChange);
 }
