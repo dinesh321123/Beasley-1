4f6750d2d774705a977625de13864c862ceae0c1
Surjit Vala <surjit.vala@bbgi.com>
2022-07-12 13:59:53 +0000

    Revert "Merge branch 'fix/same-image-calling-multiple-time-issue-updated' into 'stage3'"
    This reverts merge request !2029

--- themes/experience-engine-v2/includes/listiclecpt.php
+++ themes/experience-engine-v2/includes/listiclecpt.php
@@ -108,8 +108,8 @@ if ( ! function_exists( 'ee_get_listiclecpt_html' ) ) :
 									echo '<div class="common-mobile-ga-info track" data-location="' . esc_attr( $tracking_url ) . '"></div>';
 								}
 
-								//  $amItemImageType = '<div class="am_imagecode">' . $image_html . '</div>';
-								// 	echo $amItemImageType;
+								$amItemImageType = '<div class="am_imagecode">' . $image_html . '</div>';
+									echo $amItemImageType;
 
 									if( isset( $cpt_item_description[$index] ) && $cpt_item_description[$index] !== "" ) {
 									echo '<div class="am-meta-item-description">', apply_filters('the_content', $cpt_item_description[$index]),'</div>';
