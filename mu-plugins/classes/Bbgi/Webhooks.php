d7b813c195da8e5793ff88bf0114116d66ff63c7
riddhi.panchal <riddhi.panchal@bbgi.com>
2022-08-04 16:40:43 +0530

    Change cache tag name for the feed from the clear cache API

--- mu-plugins/classes/Bbgi/Webhooks.php
+++ mu-plugins/classes/Bbgi/Webhooks.php
@@ -353,10 +353,11 @@ class Webhooks extends \Bbgi\Module {
         $post_slug = $post->post_type.'-'.$post->post_name;
 
 		$cache_tags = [$post_slug];
+		$cache_tags[] = 'feed-'.$post_slug;
 		if ( !empty($categories)) {
 			foreach ($categories as $category) {
 				$cache_tags[] = 'archive-' . $category->slug;
-				$cache_tags[] = 'archive-' .$category->slug.'-feed';
+				$cache_tags[] = 'feed-' .$category->slug;
 			}
 		}
 
@@ -368,7 +369,7 @@ class Webhooks extends \Bbgi\Module {
 
 		if (!empty($posttype)) {
 			$cache_tags[] = 'archive-' . $posttype;
-			$cache_tags[] =  $posttype.'-feed';
+			$cache_tags[] =  'feed-'.$posttype;
 			if ($posttype == 'episode') {
 				$cache_tags[] = 'podcast';
 			}
