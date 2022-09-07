ad8a6950d79dd0851d3e7376bd6bcc89ce267174
riddhi.panchal <riddhi.panchal@bbgi.com>
2022-09-07 17:37:46 +0530

    Print response json

--- mu-plugins/classes/Bbgi/Integration/assets/js/clear-cloudflare-cache.js
+++ mu-plugins/classes/Bbgi/Integration/assets/js/clear-cloudflare-cache.js
@@ -11,6 +11,7 @@
 	$( document ).ready( function() {
 		$('.clearCloudFlareCache').on('click', function(el){
 			requestFunction('clear_cloudflare_cache',{ postID: $(this).attr('data-id') }).then((result) => {
+				alert('Cache has been cleared');
 					console.log(result)
 				})
 			.catch((error) => {
