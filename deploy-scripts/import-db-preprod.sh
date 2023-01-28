4d27b781bbc0ecd77d37aa714c556b6755cb0370
shahzaibcb <shahzaib.cb@gmail.com>
2023-01-29 01:02:56 +0500

    test rsync the db .sql from jobs to preprod

--- deploy-scripts/import-db-preprod.sh
+++ deploy-scripts/import-db-preprod.sh
@@ -6,6 +6,6 @@ set -x
 # Catch Errors
 set -euo pipefail
 
-ssh beanstalk@52.0.13.41 'rsync -vrxc preprod_dbs preprod:~/ --dry-run'
+ssh beanstalk@52.0.13.41 'rsync -vrxc preprod_backups preprod:~/ --dry-run'
 #ssh beanstalk@52.0.13.41 'pwd'
 set +x
