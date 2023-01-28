3d51d3ea23d553b18dbd6f9f3961d0fe32b0b301
shahzaibcb <shahzaib.cb@gmail.com>
2023-01-29 02:11:33 +0500

    test rsync the db .sql from jobs to preprod

--- deploy-scripts/import-db-preprod.sh
+++ deploy-scripts/import-db-preprod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+WP_PATH="/var/www/html/wordpress"
 
 # Print commands to the screen
 set -x
@@ -6,6 +7,16 @@ set -x
 # Catch Errors
 set -euo pipefail
 
-ssh beanstalk@52.0.13.41 'rsync -vrxc preprod_backups preprod:~/ --dry-run'
-#ssh beanstalk@52.0.13.41 'pwd'
+# Sync .sql files from jobs to preprod
+ssh beanstalk@52.0.13.41 'rsync -vrxc preprod_backups preprod:~/'
+
+# Import .sql files into preprod
+SITES=$(ssh beanstalk@34.230.103.178 'ls preprod_backups')
+for SITE in "${SITES}";
+do
+  #ssh beanstalk@34.230.103.178 'wp --ssh=preprod:${WP_PATH} db import ~/preprod_backups/${SITE} --path=${WP_PATH}
+  echo $SITE
+done
+
 set +x
+
