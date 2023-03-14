44a281158731248088fa381a0fbf402a67f47823
Rupesh Jorkar <rupesh.jorkar@c-metric.com>
2023-03-14 15:11:54 +0530

    Add dynamic header, footer,title, sidebar for ads like common pages

--- themes/experience-engine-v2/templates/myaccount.php
+++ themes/experience-engine-v2/templates/myaccount.php
@@ -4,17 +4,25 @@
  */
 ?>
 
-<?php
+<?php get_header(); ?>
 
-get_header();
+<?php the_post(); ?>
 
-the_post();
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="post-info">
+		<?php if ( bbgi_featured_image_layout_is( null, 'top' ) ) : ?>
+			<?php get_template_part( 'partials/featured-media' ); ?>
+		<?php endif; ?>
 
-echo '<div class="', join( ' ', get_post_class() ), '">';
-echo '<div class="content-wrap">';
-ee_the_subtitle('My Account Information');
-echo do_shortcode('[cancel_account]');
-echo '</div>';
-echo '</div>';
+		<h1><?php the_title(); ?></h1>
+	</header>
 
-get_footer();
+	<div class="entry-content content-wrap">
+		<?php get_template_part( 'partials/page/description' ); ?>
+		<?php echo do_shortcode('[cancel_account]'); ?>
+		<?php get_template_part( 'partials/footer/common', 'description' ); ?>
+		<?php get_template_part( 'partials/ads/sidebar-sticky' ); ?>
+	</div>
+</div>
+
+<?php get_footer(); ?>
