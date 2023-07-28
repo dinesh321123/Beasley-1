b8476bc871ff6c2197823c7ef10ad1da6d464615
riddhi.panchal <riddhi.panchal@bbgi.com>
2023-07-28 19:35:35 +0530

    troubleshooting author feed page

--- plugins/cpt-general-settings/includes/rendering.php
+++ plugins/cpt-general-settings/includes/rendering.php
@@ -82,12 +82,15 @@ class GeneralSettingsFrontRendering {
 		
 		global $wp;
 		$author_string = $wp->query_vars['author_name'];
-		// var_dump($author_string);
-		// var_dump(!is_feed());
+
+		echo '<script>'.$author_string.'</script>';
+
 		if($author_string != ''){
 			if(!is_feed()){
 				$author = get_user_by('slug', $author_string);
-				// var_dump(!$author);
+				
+				print_r($author);
+
 				if(!$author){
 					// ee_404_page_redirect();
 					global $wp_query;
