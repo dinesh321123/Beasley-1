2748600e2c9933929df4159933921263dc3a9669
Parth Shah <parthshah@macs-Mac-mini.local>
2023-09-04 18:11:15 +0530

    css fix for slimmer sub-navigation

--- themes/experience-engine-v2/single-show.php
+++ themes/experience-engine-v2/single-show.php
@@ -5,14 +5,18 @@ get_header();
 the_post();
 
 echo '<div class="', join( ' ', get_post_class() ), '">';
-	echo '<div class="content-wrap">';
-		if ( ee_is_first_page() ) :			
-			get_template_part( 'partials/show/custom-header' );
-			get_template_part( 'partials/show/featured' );
-			get_template_part( 'partials/show/favorites' );
-		endif;
+	echo '<div class="article-inner-container">';
+		echo '<div class="entry-content content-wrap">';
+			echo '<div class="content-wrap">';
+				if ( ee_is_first_page() ) :			
+					get_template_part( 'partials/show/custom-header' );
+					get_template_part( 'partials/show/featured' );
+					get_template_part( 'partials/show/favorites' );
+				endif;
 
-		get_template_part( 'partials/show/recent' );
+				get_template_part( 'partials/show/recent' );
+			echo '</div>';
+		echo '</div>';
 	echo '</div>';
 echo '</div>';
 
